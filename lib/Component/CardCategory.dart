64d815c840fc41e7804c0ddff96f2ee52edd0d88
George Abraham Talakua <87080055+abrahamgeorge325@users.noreply.github.com>
2023-06-22 17:43:02 +0700

    Tambah Landing Page

---
+++ lib/Component/CardCategory.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class CardCategory extends StatelessWidget {
+  //final String imageCategory;
+  //final String nameCategory;
+
+  //CardCategory({this.imageCategory, this.nameCategory});
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Image.asset('assets/images/flu.png',
+          width: 50,
+          height: 50,
+        ),
+        Text('Flu',style: TextStyle(fontWeight: FontWeight.bold)),
+      ],
+    );
+  }
+}
