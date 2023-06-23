89d06ccc57856a75346e67b9154548946d2f2516
George Abraham Talakua <87080055+abrahamgeorge325@users.noreply.github.com>
2023-06-23 15:42:25 +0700

    Add CardProduct n List_Obat

---
+++ lib/Page/List_Obat.dart
@@ -0,0 +1,93 @@
+import 'package:apobat/Component/CardProduct.dart';
+import 'package:flutter/material.dart';
+
+class List_Obat extends StatefulWidget {
+  const List_Obat({Key? key}) : super(key: key);
+
+  @override
+  State<List_Obat> createState() => _List_ObatState();
+}
+
+class _List_ObatState extends State<List_Obat> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            label: 'Beranda',
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            label: 'Keranjang',
+            icon: Icon(Icons.shopping_basket),
+          ),
+          BottomNavigationBarItem(
+            label: 'Akun',
+            icon: Icon(Icons.manage_accounts),
+          )
+        ],
+      ),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            floating: true,
+            pinned: true,
+            snap: false,
+            centerTitle: true,
+            title: const Text('APOBAT'),
+            leading: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Image.asset(
+                "lib/Image/pills.png",
+              ),
+            ),
+            actions: [
+              IconButton(
+                icon: const Icon(Icons.medical_services),
+                onPressed: () {},
+              ),
+            ],
+            bottom: AppBar(
+              title: Container(
+                width: double.infinity,
+                height: 40,
+                color: Colors.white,
+                child: const Center(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Cari Obat',
+                      prefixIcon: Icon(Icons.search),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          // Other Sliver Widgets
+          SliverList(
+            delegate: SliverChildListDelegate([
+              const SizedBox(
+                height: 85,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('\n\n\t\t\t\tObat Flu',style: TextStyle(fontSize: 16,
+                            fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              CardProduct()
+            ]),
+          ),
+        ],
+      ),
+    );
+  }
+}
